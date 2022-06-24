166053151f2791dcbd5c9942de0dfe4e54b241df
rachmagit <rachmawatioja@gmail.com>
2022-06-25 01:06:06 +0530

    Recyclerview APP

---
+++ app/src/main/java/com/rachmawatioja/recyclerview/model/Pemain.kt
@@ -0,0 +1,12 @@
+package com.rachmawatioja.recyclerview.model
+
+import java.util.stream.StreamSupport
+
+data class Pemain(
+    var nama : String,
+    var foto : Int,
+    var posisi : String,
+    var tinggi : String,
+    var tempatlahir : String,
+    var tgllahir : String
+)
